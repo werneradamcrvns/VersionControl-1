b9f29326455ab66ba73907daa2d32aac982853b2
Áron Kósa <kosa.aron98@gmail.com>
2019-11-23 10:46:16 +0100

    Creating Form1 design

---
+++ UserMaintenance/UserMaintenance/Entities/User.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UserMaintenance.Entities
+{
+    class User
+    {
+        public Guid ID { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string FullName
+        {
+            get
+            {
+                return string.Format(
+                    "{0} {1}",
+                    FirstName, LastName);
+            }
+        }
+
+    }
--- UserMaintenance/UserMaintenance/Form1.cs
+++ UserMaintenance/UserMaintenance/Form1.cs
@@ -15,6 +15,10 @@ namespace UserMaintenance
         public Form1()
         {
             InitializeComponent();
+            lblFirstName.Text = Resource1.FirstName;
+            lblLastName.Text = Resource1.LastName;
+            btnAdd.Text = Resource1.Add;
+
         }
     }
 }
